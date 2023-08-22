0b7ada6abc2b1ebec5c3d1e84f98415832ea604f
Flidix <marko.lazarchuk@gmail.com>
2023-08-22 15:28:28 +0300

    email page update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 
